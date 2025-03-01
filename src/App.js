1234861f8cad2bb2fcf00f94f60696fef02209c6
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-01 08:26:31 +0100

    App 4

--- src/App.js
+++ src/App.js
@@ -59,15 +59,6 @@ export const ThemeProvider = ({ children }) => {
 
 export const useTheme = () => useContext(ThemeContext);
 
-
-// Contact.jsx
-function Contact() {
-  const { t } = useTranslation();
-  return <h1 className="text-3xl text-center">{t('contact')}</h1>;
-}
-
-export default Contact;
-
 // i18n.js (Langues)
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
