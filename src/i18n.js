792b3b40ca23717c7a4e92c481c518672dd3be0d
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-01 17:34:34 +0100

    Structured

--- src/i18n.js
+++ src/i18n.js
@@ -3,10 +3,43 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 const resources = {
-  en: { translation: { home: 'Home', about: 'About', contact: 'Contact', footer: 'All rights reserved' } },
-  fr: { translation: { home: 'Accueil', about: 'À Propos', contact: 'Contact', footer: 'Tous droits réservés' } },
-  es: { translation: { home: 'Inicio', about: 'Sobre Nosotros', contact: 'Contacto', footer: 'Todos los derechos reservados' } },
-  de: { translation: { home: 'Startseite', about: 'Über uns', contact: 'Kontakt', footer: 'Alle Rechte vorbehalten' } },
+  en: 
+    { 
+      translation: 
+      { 
+        home: 'Home', 
+        about: 'About', 
+        contact: 'Contact', 
+        footer: 'All rights reserved' 
+      } 
+    },
+  fr: { 
+    translation: 
+    { 
+      home: 'Accueil', 
+      about: 'À Propos', 
+      contact: 'Contact', 
+      footer: 'Tous droits réservés' 
+    } 
+  },
+  es: { 
+    translation: 
+    { 
+      home: 'Inicio', 
+      about: 'Sobre Nosotros', 
+      contact: 'Contacto', 
+      footer: 'Todos los derechos reservados' 
+    } 
+  },
+  de: { 
+    translation: 
+    { 
+      home: 'Startseite', 
+      about: 'Über uns', 
+      contact: 'Kontakt', 
+      footer: 'Alle Rechte vorbehalten' 
+    } 
+  },
 };
 
 i18n.use(initReactI18next).init({
